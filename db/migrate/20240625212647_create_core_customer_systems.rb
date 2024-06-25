02b04268d53b9da7e02a8da622fb5cc5a2893edc
pham.thanh.nam <phamthanhnam@Namss-MBP.lan>
2024-06-26 05:59:07 +0700

    FIx data type

--- db/migrate/20240625212647_create_core_customer_systems.rb
+++ db/migrate/20240625212647_create_core_customer_systems.rb
@@ -1,7 +1,7 @@
 class CreateCoreCustomerSystems < ActiveRecord::Migration[7.0]
   def change
     create_table :core_customer_systems do |t|
-      t.text :base64
+      t.text :base64, limit: 0.3.gigabytes
 
       t.timestamps
     end
