6efef59eedd715d4fc7fe76017de930ead07878e
pham.thanh.namb <SUN-ASTERISK\pham.thanh.namb@b123121-pc.sun-asterisk.com>
2024-07-15 18:22:33 +0700

    Add v2

--- app/controllers/api/v1/app_informations_controller.rb
+++ app/controllers/api/v1/app_informations_controller.rb
@@ -4,6 +4,7 @@
 class Api::V1::AppInformationsController < Api::V1::ApplicationControllerPublic
   def index
     # Load default app information
+    # TEST
     @app_information = AppInformation.first
 
     render json: @app_information, each_serializer: Api::V1::AppInformationSerializer
