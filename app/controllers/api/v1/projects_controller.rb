724ffbeba44316d69c9d9164259b4a3cc7f548c2
phamnaminter <57590319+phamnaminter@users.noreply.github.com>
2024-07-30 17:54:05 +0700

    Update projects_controller.rb

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -5,6 +5,6 @@ class Api::V1::ProjectsController < Api::V1::ApplicationControllerPublic
   def index
     @projects = Project.all
 
-    render json: {version: 2}
+    render json: {version: 22}
   end
 end
